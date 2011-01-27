a3111e84016d8944cee985b1e60d1ffb4b850bce
shabiel <sam.habiel@gmail.com>
2011-01-27 12:34:22 +0000

    Minor oopsie. Fixed.

--- cs/bmx_0200scr/BMX2/BMXNet/AssemblyInfo.cs
+++ cs/bmx_0200scr/BMX2/BMXNet/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("BMXNet22")]
+[assembly: AssemblyTitle("BMXNet23")]
 [assembly: AssemblyDescription("ADO.NET 2.0 Data Provider for VISTA")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WorldVistA")]
